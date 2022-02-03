6e3631d91d050e1969c6b238afba9e9104f3c7bb
greatw1te2 <corbingarlough@cmail.carleton.ca>
2022-02-03 11:22:42 -0500

    Update dbconfig.py

--- dbconfig.py
+++ dbconfig.py
@@ -1,10 +1,10 @@
-email="firstnamelastname@cmail.carleton.ca'
-firstname="firstname"
-lastname="lastname"
+email="corbingarlough@cmail.carleton.ca'
+firstname="Corbin"
+lastname="Garlough"
 
 config = dict(
-    apiKey = "myapikey",
-    authDomain = "<myproject>.firebase.com",
-    databaseURL = "<myurl>.firebaseio.com/",
-    storageBucket = "<mystorage>.appspot.com"
+    apiKey = "AIzaSyDzXobt8eQ5G5U36GcIawn_lGmAqPz8DNI",
+    authDomain = "sysc3010lab4.firebase.com",
+    databaseURL = "sysc3010lab4.firebaseio.com/",
+    storageBucket = "gs://sysc3010lab4.appspot.com"
 )
